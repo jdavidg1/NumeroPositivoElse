95a3127afe288bcb13392d4a100334552e74bf79
jdavidg1 <jdavidg_1@hotmail.com>
2019-06-18 17:18:06 -0500

    Programa final

---
+++ NumeroPositivoElse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroPositivoElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero = 0;
+
+            Console.WriteLine("Escribe un número para definir si es positivo o negativo");
+
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero >= 0)
+            {
+                Console.WriteLine("El numero {0} es mayor que cero", numero);
+            }
+
+            else
+            {
+                Console.WriteLine("El número {0} es menor que cero", numero);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
